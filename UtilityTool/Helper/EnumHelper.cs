4ca596442dbd0b8f1e78c54764889d56c67fd800
chuangmaster <masterchuang@urtody.com>
2019-08-19 10:43:01 +0800

    加入EnumHelper

---
+++ UtilityTool/Helper/EnumHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityTool.Helper
+{
+    public static class EnumHelper
+    {
+
+        public static Dictionary<int, string> GetAllMemberDescription<T>(T t)
+        where T : Type
+        {
+            if (!t.IsEnum)
+            {
+                throw new Exception("Is Not Enum");
+            }
+            var result = new Dictionary<int, string>();
+            
+            var enumArr = System.Enum.GetValues(t);
+            foreach (var e in enumArr)
+            {
+                var members = t.GetMember(e.ToString());
+                foreach (var m in members)
+                {
+                    var attr = m.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    if (attr != null)
+                    {
+                        result.Add((int)e, attr.ToString());
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}

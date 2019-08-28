81e3f7e88f50b9a4c3a4bffffe2426405525d9a7
chuangmaster <masterchuang@urtody.com>
2019-08-28 18:18:42 +0800

    加入AsyncHelper

---
+++ UtilityTool/Helper/AsyncHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UtilityTool.Helper
+{
+    /// <summary>
+    /// Async Helper
+    /// </summary>
+    public class AsyncHelper
+    {
+        private static readonly TaskFactory _factory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        public static void RunSync(Func<Task> func)
+        {
+            _factory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return _factory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+    }
+}

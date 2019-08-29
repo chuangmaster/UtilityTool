e1d4812e083fa7b493d227d3419687cb13d29d0f
chuangmaster <masterchuang@urtody.com>
2019-08-29 13:15:19 +0800

    加入註解

--- UtilityTool/Helper/AsyncHelper.cs
+++ UtilityTool/Helper/AsyncHelper.cs
@@ -14,11 +14,21 @@ namespace UtilityTool.Helper
     {
         private static readonly TaskFactory _factory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
 
+        /// <summary>
+        /// 執行非同步無回傳值
+        /// </summary>
+        /// <param name="func"></param>
         public static void RunSync(Func<Task> func)
         {
             _factory.StartNew(func).Unwrap().GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// 執行非同步有回傳值
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
         public static TResult RunSync<TResult>(Func<Task<TResult>> func)
         {
             return _factory.StartNew(func).Unwrap().GetAwaiter().GetResult();

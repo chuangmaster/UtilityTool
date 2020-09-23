27d63e840535e52878db0923b683b704ceb39dce
chuangmaster <masterchuang@urtody.com>
2020-09-23 14:35:43 +0800

    add aes encrypt and decrypt method; add sha hash method

---
+++ UtilityTool/EncryptTool.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityTool
+{
+    public static class EncryptTool
+    {
+        /// <summary>
+        /// 字串加密(非對稱式)
+        /// </summary>
+        /// <param name="Source">加密前字串</param>
+        /// <param name="CryptoKey">加密金鑰</param>
+        /// <returns>加密後字串</returns>
+        public static string AesEncryptBase64(string SourceStr, string CryptoKey)
+        {
+            string encrypt = "";
+            try
+            {
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                aes.Key = key;
+                aes.IV = iv;
+
+                byte[] dataByteArray = Encoding.UTF8.GetBytes(SourceStr);
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(dataByteArray, 0, dataByteArray.Length);
+                    cs.FlushFinalBlock();
+                    encrypt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return encrypt;
+        }
+
+        /// <summary>
+        /// 字串解密(非對稱式)
+        /// </summary>
+        /// <param name="Source">解密前字串</param>
+        /// <param name="CryptoKey">解密金鑰</param>
+        /// <returns>解密後字串</returns>
+        public static string AesDecryptBase64(string SourceStr, string CryptoKey)
+        {
+            string decrypt = "";
+            try
+            {
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                aes.Key = key;
+                aes.IV = iv;
+
+                byte[] dataByteArray = Convert.FromBase64String(SourceStr);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(dataByteArray, 0, dataByteArray.Length);
+                        cs.FlushFinalBlock();
+                        decrypt = Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return decrypt;
+        }
+
+        /// <summary>
+        /// 取得使用SHA256進行雜湊結果
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetSHA256Hash(string str)
+        {
+            SHA256 sha = new SHA256Managed();
+            byte[] byteData = Encoding.Default.GetBytes(str);
+            return BitConverter.ToString(sha.ComputeHash(byteData)).Replace("-", string.Empty);
+        }
+    }
+}
